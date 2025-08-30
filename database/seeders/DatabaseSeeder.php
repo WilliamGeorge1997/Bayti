9a6313187ba3075bcca4cd209905cd79983e7534
William George <williamgeorge1997x@gmail.com>
2025-08-30 18:01:06 +0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // AdminDatabaseSeeder::class,
+            AdminDatabaseSeeder::class,
             // CategoryDatabaseSeeder::class,
             CountryDatabaseSeeder::class,
         ]);
